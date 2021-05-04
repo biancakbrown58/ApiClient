c972f3e4b6044b64ee8267f07ad5cf70c4a7f39c
Bianca Brown <biancakbrown58@gmail.com>
2021-05-04 12:44:42 -0400

    Able to get results back from the api

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Item
     {
-      Console.WriteLine("Welcome to C#");
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        [JsonPropertyName("setup")]
+        public string Setup { get; set; }
+        [JsonPropertyName("punchline")]
+        public string Punchline { get; set; }
     }
-  }
+
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var url = $"https://official-joke-api.appspot.com/jokes/programming/random";
+            var responseAsStream = await client.GetStreamAsync(url);
+
+            var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"Want to hear a joke? {item.Setup}");
+                Console.WriteLine($"{item.Punchline}");
+            }
+        }
+    }
 }
+
+// [
+//   {
+//     "id": 33,
+//     "type": "programming",
+//     "setup": "Which song would an exception sing?",
+//     "punchline": "Can't catch me - Avicii"
+//   }
+// ]
